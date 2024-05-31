f8af6515e3bfc0100e2c33e7783a5f70ca358456
maenotakanori <115054954+maenotakanori@users.noreply.github.com>
2024-05-31 10:36:32 +0900

    02_04完了

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -36,31 +36,31 @@ void GameScene::Initialize() {
 
 	modelBlock_ = Model::CreateFromOBJ("cube");
 
-	// 要素数
-	const uint32_t kNumBlockVirtical = 10;
-	const uint32_t kNumBlockHorizontal = 20;
-	// ブロック1個分の横幅
-	const float kBlockWidth = 2.0f;
-	const float kBlockHeight = 2.0f;
-	// 要素数を変更する
-	// 列数を設定（縦方向のブロック数）
-	worldTransformBlocks_.resize(kNumBlockVirtical);
-	for (uint32_t i = 0; i < kNumBlockVirtical; i++) {
-		// 1列の要素数を設定（横方向のブロック数）
-		worldTransformBlocks_[i].resize(kNumBlockHorizontal);
-	}
+	//// 要素数
+	//const uint32_t kNumBlockVirtical = 10;
+	//const uint32_t kNumBlockHorizontal = 20;
+	//// ブロック1個分の横幅
+	//const float kBlockWidth = 2.0f;
+	//const float kBlockHeight = 2.0f;
+	//// 要素数を変更する
+	//// 列数を設定（縦方向のブロック数）
+	//worldTransformBlocks_.resize(kNumBlockVirtical);
+	//for (uint32_t i = 0; i < kNumBlockVirtical; i++) {
+	//	// 1列の要素数を設定（横方向のブロック数）
+	//	worldTransformBlocks_[i].resize(kNumBlockHorizontal);
+	//}
 
-	// キューブの生成
-	for (uint32_t i = 0; i < kNumBlockVirtical; ++i) {
-		for (uint32_t j = 0; j < kNumBlockHorizontal; ++j) {
-			if ((i + j) % 2 == 0)
-				continue;
-			worldTransformBlocks_[i][j] = new WorldTransform();
-			worldTransformBlocks_[i][j]->Initialize();
-			worldTransformBlocks_[i][j]->translation_.x = kBlockWidth * j;
-			worldTransformBlocks_[i][j]->translation_.y = kBlockHeight * i;
-		}
-	}
+	//// キューブの生成
+	//for (uint32_t i = 0; i < kNumBlockVirtical; ++i) {
+	//	for (uint32_t j = 0; j < kNumBlockHorizontal; ++j) {
+	//		if ((i + j) % 2 == 0)
+	//			continue;
+	//		worldTransformBlocks_[i][j] = new WorldTransform();
+	//		worldTransformBlocks_[i][j]->Initialize();
+	//		worldTransformBlocks_[i][j]->translation_.x = kBlockWidth * j;
+	//		worldTransformBlocks_[i][j]->translation_.y = kBlockHeight * i;
+	//	}
+	//}
 
 	debugCamera_ = new DebugCamera(1280, 720);
 
